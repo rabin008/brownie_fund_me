4f1d1b8072f2e62d084c35b76faf76fd3a3a3bd7
user.email <rabin008@hotmail.com>
2022-05-08 17:35:27 -0500

    update

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -9,13 +9,18 @@ def fund():
     print(entrance_fee)
     print(f"The current entry fee is {entrance_fee}")
     print("Funding")
-    fund_me.fund({"from": account, "value": entrance_fee})
+    fund_me = fund_me.fund({"from": account, "value": entrance_fee})
+    fund_me.wait(1)
+    print("Funded")
 
 
 def withdraw():
     fund_me = FundMe[-1]
     account = get_account()
-    fund_me.withdraw({"from": account})
+    print("withdrawing")
+    withdraw = fund_me.withdraw({"from": account})
+    withdraw.wait(1)
+    print("End")
 
 
 def main():
